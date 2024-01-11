75cf8f76e5af9b4a7e21f1f21616ed4495589f1e
Gurman Othi <othigurman@gmail.com>
2024-01-10 22:16:37 -0500

    Response Curve now Responds to Change

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -156,6 +156,11 @@ void SimpleEQAudioProcessorEditor::timerCallback()
         auto peakCoefficients = makePeakFilter(chainSettings, audioProcessor.getSampleRate());
         updateCoefficients(monoChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
         
+        auto lowCutCoefficients = makeLowCutFliter(chainSettings, audioProcessor.getSampleRate());
+        auto highCutCoefficients = makeHighCutFliter(chainSettings, audioProcessor.getSampleRate());
+        
+        updateCutFilter(monoChain.get<ChainPositions::LowCut>(), lowCutCoefficients, chainSettings.lowCutSlope);
+        updateCutFilter(monoChain.get<ChainPositions::HighCut>(), highCutCoefficients, chainSettings.highCutSlope);
         repaint();
     }
 }
