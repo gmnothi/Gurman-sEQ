f94126cc035d4a0bc3babe24a27125602a73b006
Gurman Othi <othigurman@gmail.com>
2024-01-10 20:58:47 -0500

    Refactoring

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -223,17 +223,24 @@ ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts)
     return settings;
 }
 
+Coefficients makePeakFilter(const ChainSettings& chainSettings, double sampleRate)
+{
+    return juce::dsp::IIR::Coefficients<float>::makePeakFilter(sampleRate, chainSettings.peakFreq, chainSettings.peakQuality, juce::Decibels::decibelsToGain(chainSettings.peakGainInDecibels));
+}
+
 void SimpleEQAudioProcessor::updatePeakFliter(const ChainSettings &chainSettings)
 {
-    auto peakcoefficients = juce::dsp::IIR::Coefficients<float>::makePeakFilter(getSampleRate(), chainSettings.peakFreq, chainSettings.peakQuality, juce::Decibels::decibelsToGain(chainSettings.peakGainInDecibels));
+   // auto peakcoefficients = juce::dsp::IIR::Coefficients<float>::makePeakFilter(getSampleRate(), chainSettings.peakFreq, chainSettings.peakQuality, juce::Decibels::decibelsToGain(chainSettings.peakGainInDecibels));
     
  //   *leftChain.get<ChainPositions::Peak>().coefficients = *peakcoefficients;
 //    *rightChain.get<ChainPositions::Peak>().coefficients = *peakcoefficients;
-    updateCoefficients(leftChain.get<ChainPositions::Peak>().coefficients,peakcoefficients);
-    updateCoefficients(rightChain.get<ChainPositions::Peak>().coefficients,peakcoefficients);
+    auto peakCoefficients = makePeakFilter(chainSettings, getSampleRate());
+    
+    updateCoefficients(leftChain.get<ChainPositions::Peak>().coefficients,peakCoefficients);
+    updateCoefficients(rightChain.get<ChainPositions::Peak>().coefficients,peakCoefficients);
 }
 
-void SimpleEQAudioProcessor::updateCoefficients(Coefficients &old, const Coefficients & replacements)
+void updateCoefficients(Coefficients &old, const Coefficients & replacements)
 {
     *old = *replacements;
 }
--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -41,6 +41,10 @@ enum ChainPositions
     HighCut
 };
 
+using Coefficients = Filter::CoefficientsPtr;
+void updateCoefficients(Coefficients& old, const Coefficients& replacements);
+
+Coefficients makePeakFilter(const ChainSettings& chainSettings, double sampleRate);
 //==============================================================================
 /**
 */
@@ -94,8 +98,7 @@ private:
     
     
     void updatePeakFliter(const ChainSettings& chainSettings);
-    using Coefficients = Filter::CoefficientsPtr;
-    static void updateCoefficients(Coefficients& old, const Coefficients& replacements);
+    
     
     template<int Index, typename ChainType, typename CoefficientType>
     void update(ChainType& chain, const CoefficientType& coefficients)
