6cc743b40c298d873e8326b1dd74d67db97bd908
Gurman Othi <othigurman@gmail.com>
2024-01-10 20:53:54 -0500

    Further Edits

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -130,6 +130,19 @@ void SimpleEQAudioProcessorEditor::resized()
     peakQualitySlider.setBounds(bounds);
 }
 
+void SimpleEQAudioProcessorEditor::parameterValueChanged(int parameterIndex, float newValue)
+{
+    parametersChanged.set(true);
+}
+
+void SimpleEQAudioProcessorEditor::timerCallback()
+{
+    if(parametersChanged.compareAndSetBool(false, true))
+    {
+        
+    }
+}
+
 std::vector<juce::Component*> SimpleEQAudioProcessorEditor::getComps()
 {
     return 
